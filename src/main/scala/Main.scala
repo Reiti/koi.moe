ba0be434b87c2a701e87f934efb48e99a1e07d42
Michael Reitgruber <reitgruber.michael@gmail.com>
2016-02-07 14:32:55 +0100

    adds ability to server static images

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,31 @@
+import com.twitter.io.{Reader, Buf}
 import io.finch.{head => _, body => _, _}
-import com.twitter.finagle.Http
+import com.twitter.finagle._
+import com.twitter.finagle.http.{Request, Response}
 import scalatags.Text._
 import scalatags.Text.short._
+import com.twitter.util.Await
 
 
 object Main extends App {
 
-  val api: Endpoint[String] = get(/) {
+  val welcome: Endpoint[String] = get(/) {
     Ok(
       html(
         head(tags2.title("Awoo")),
         body(
-          img(attrs.src := "https://i.imgur.com/JJ0S1Wm.png")
+          img(attrs.src := "/img/awoo.png")
         )
       ).render
   ).withContentType(Some("text/html")) }
 
 
-  Http.serve("0.0.0.0:80", api.toService)
+  val resources: Endpoint[Buf] = get("img" / string) { (name: String) =>
+      val reader: Reader = Reader.fromStream(getClass.getClassLoader.getResourceAsStream(name))
+      Ok(Reader.readAll(reader)).withContentType(Some("image/png"))
+  }
+
+  val api: Service[Request, Response] = (welcome :+: resources).toService
+
+  Await.ready(Http.serve("0.0.0.0:80", api))
 }
